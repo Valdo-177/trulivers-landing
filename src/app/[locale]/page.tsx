807295cb12568cfd7ad15ccf89f43f92e2c49633
Valdo-117 <keyerdelahozsteam@gmail.com>
2025-03-12 12:18:50 -0500

    fix: add buttom google and app store

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -19,7 +19,10 @@ import salamanca from "@/assets/img/salamanca.png"
 import autonoma from "@/assets/img/autonoma.png"
 import emprende from "@/assets/img/emprende.png"
 import italiano from "@/assets/img/ponte.png"
+import appStore from "@/assets/svg/appstoreTrulivers.svg"
+import googlePlay from "@/assets/svg/googleplayTrulivers.svg"
 import Script from 'next/script'
+import Link from "next/link";
 
 
 function WordsPullUp({
@@ -126,15 +129,20 @@ export default function Home() {
         <section className="sm:h-[50rem] h-[40rem] flex items-center bgHero">
           <div className="flex flex-col gap-6 lg:w-[46rem] md:w-[36rem]">
             <span className="text-primaryBg uppercase text-2xl font-semibold leading-[1.3rem]">{heroTranslate("subTitle")}</span>
-            {/* <h1 className="text-[#3D3D3D] text-4xl lg:text-5xl font-extrabold sm:leading-[3.5rem] leading-[2.1rem]">{heroTranslate("title")}</h1> */}
             <div className="max-w-[45rem]">
               <WordsPullUp text={heroTranslate("title")} className="text-[#3D3D3D] text-4xl lg:text-5xl font-extrabold sm:leading-[3.5rem] leading-[2.1rem] uppercase" />
             </div>
             <p className="text-[#525252] leading-7 sm:w-[30rem]">{heroTranslate("description1")} <strong>{heroTranslate("description2")}</strong> {heroTranslate("description3")}</p>
-            <div className="flex items-center gap-5">
+            <div className="flex items-center gap-4">
               {/* <Button className='bg-primaryBg md:bg-red-600 lg:bg-blue-500 xl:bg-green-400 2xl:bg-slate-500  hover:bg-[#0B0DC2] rounded-[50px] h-[43px]'>{translate("btn")}</Button> */}
-              <Button className='bg-primaryBg hover:bg-[#0B0DC2] rounded-[50px] h-[43px]'>{heroTranslate("btn1")}</Button>
-              <Button className='bg-primaryBg hover:bg-[#0B0DC2] rounded-[50px] h-[43px]'>{heroTranslate("btn2")}</Button>
+              <Button className='bg-primaryBg hover:bg-[#0B0DC2] h-[43px]'>{heroTranslate("btn1")}</Button>
+              <Link href='https://apps.apple.com/co/app/trulivers/id6446598356' target="_blank">
+                <Image src={appStore} alt="Boton de App Store" className="h-[43px] w-max" />
+              </Link>
+              <Link href='https://play.google.com/store/apps/details?id=com.truliapp&hl=es_419' target="_blank">
+                <Image src={googlePlay} alt="Boton de App Store" className="h-[43px] w-max" />
+              </Link>
+              {/* <Button className='bg-primaryBg hover:bg-[#0B0DC2] rounded-[50px] h-[43px]'>{heroTranslate("btn2")}</Button> */}
             </div>
           </div>
         </section>
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,5 @@
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 import React from 'react'
 
 const Footer = () => {
@@ -10,8 +11,24 @@ const Footer = () => {
                 <div>
                     <div className='flex flex-col items-start'>
                         <h2 className='text-lg text-primaryBg font-bold mb-3'>TRULIVERS</h2>
-                        <span className='font-light text-sm'>{translate("item1")}: (+57) 3013735256</span>
-                        <span className='font-light text-sm'>{translate("item2")}:---------</span>
+                        <span className='font-light text-sm'>{translate("item1")}:
+                            <Link
+                                href="https://wa.me/573013735256?text=Hola,%20quisiera%20más%20información."
+                                target="_blank"
+                                className='font-medium ml-1 hover:underline'
+                            >
+                                (+57) 3013735256
+                            </Link>
+                        </span>
+                        <span className='font-light text-sm'>{translate("item2")}:
+                            <Link
+                                href="https://mail.google.com/mail/?view=cm&fs=1&to=info@trulivers.com&su=Consulta&body=Hola, quisiera más información."
+                                target="_blank"
+                                className='font-medium ml-1 hover:underline'
+                            >
+                                info@trulivers.com
+                            </Link>
+                        </span>
                     </div>
                 </div>
                 <div>
